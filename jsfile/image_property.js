3edbef5e33738c19232fe7d2527d94f7937cb44e
kaungkyaw <aungakk619@gmail.com>
2023-04-29 19:49:57 +0630

    4/29/2023

---
+++ jsfile/image_property.js
@@ -0,0 +1,7 @@
+function change_background(){
+    document.querySelector('.heading_1').style.backgroundColor = "red"
+    console.log("hello")
+}
+function change_background_origin(){
+    document.querySelector('.heading_1').style.backgroundColor = "#7D7ABC"
+}
