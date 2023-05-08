9959d276a60d208325e9adcf8a0566f3c5212133
kaungkyaw <aungakk619@gmail.com>
2023-05-09 00:08:36 +0630

    5/9/2023

--- jsfile/image_property.js
+++ jsfile/image_property.js
@@ -1,7 +1,16 @@
-function change_background(){
-    document.querySelector('.heading_1').style.backgroundColor = "red"
-    console.log("hello")
+// style.change_background
+function change_background(element){
+    console.log(element)
+    element.style.backgroundColor="tomato"
 }
-function change_background_origin(){
-    document.querySelector('.heading_1').style.backgroundColor = "#7D7ABC"
+
+function change_background_origin(element){
+    element.style.backgroundColor="#7D7ABC"
+
+}
+function change_background2() {
+    document.querySelector('.heading_4').style.backgroundColor = "tomato"
+}
+function change_background2_origin(){
+    document.querySelector('.heading_4').style.backgroundColor = "#7D7ABC"
 }
